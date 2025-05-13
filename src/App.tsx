77f296bb1433f52630aad0386b16d56cf8bcc749
yrankineNEU <rankine.y@northeastern.edu>
2025-05-13 18:39:00 -0400

    hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Labs from "./Labs";
-import { HashRouter } from "react-router-dom";
-
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 export default function App() {
   return (
     <HashRouter>
       <div>
-        <Labs />
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+        </Routes>
       </div>
     </HashRouter>
   );
