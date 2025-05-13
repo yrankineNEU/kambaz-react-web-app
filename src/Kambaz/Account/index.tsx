0c53a0131c6ed237d481f8106ecd05a77b5bf630
yrankineNEU <rankine.y@northeastern.edu>
2025-05-13 19:06:46 -0400

    connect routes to account in Kambaz

--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -1,9 +1,19 @@
+import { Routes, Route, Navigate } from "react-router";
+
 import Signin from "./Signin";
+import Profile from "./Profile";
+import Signup from "./Signup";
+
 export default function Account() {
   return (
     <div id="wd-account-screen">
       <h2>Account</h2>
-      <Signin />
+      <Routes>
+        <Route path="/" element={<Navigate to="/Kambaz/Account/Signin" />} />
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Signup" element={<Signup />} />
+      </Routes>{" "}
     </div>
   );
 }
