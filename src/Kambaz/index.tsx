7b057e8f90a148464efde2601441b62eca662ddd
yrankineNEU <rankine.y@northeastern.edu>
2025-05-13 19:08:58 -0400

    make account screen default

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -4,7 +4,10 @@ export default function Kambaz() {
   return (
     <div id="wd-kambaz">
       <h1>Kambaz</h1>
-      <Account />
+      <Routes>
+        <Route path="/" element={<Navigate to="Account" />} />
+        <Route path="/Account/*" element={<Account />} />
+      </Routes>
     </div>
   );
 }
