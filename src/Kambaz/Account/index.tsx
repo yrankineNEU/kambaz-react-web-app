2dd9d54c6da814611f9e08d890444d7c6a2b1fcd
yrankineNEU <rankine.y@northeastern.edu>
2025-05-13 18:59:31 -0400

    add account sign in

---
+++ src/Kambaz/Account/index.tsx
@@ -0,0 +1,9 @@
+import Signin from "./Signin";
+export default function Account() {
+  return (
+    <div id="wd-account-screen">
+      <h2>Account</h2>
+      <Signin />
+    </div>
+  );
+}
--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -1,7 +1,10 @@
+import { Routes, Route, Navigate } from "react-router";
+import Account from "./Account";
 export default function Kambaz() {
   return (
     <div id="wd-kambaz">
       <h1>Kambaz</h1>
+      <Account />
     </div>
   );
 }
