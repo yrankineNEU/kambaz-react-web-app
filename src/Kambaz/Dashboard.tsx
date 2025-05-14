a6f4f5d5169f33d933a6653c671428d34e2c21d6
yrankineNEU <rankine.y@northeastern.edu>
2025-05-13 20:04:51 -0400

    add course navigation

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -14,7 +14,7 @@ export default function Dashboard() {
             <div>
               <h5> CS1234 React JS </h5>
               <p className="wd-dashboard-course-title">
-                Full Stack software developer{" "}
+                Full Stack software developer
               </p>
               <button> Go </button>
             </div>
@@ -29,13 +29,87 @@ export default function Dashboard() {
             <div>
               <h5> CS1235 HumanComputer Interaction </h5>
               <p className="wd-dashboard-course-title">
-                Usability and User Experience{" "}
+                Usability and User Experience
               </p>
               <button> Go </button>
             </div>
-          </Link>{" "}
+          </Link>
         </div>
-        <div className="wd-dashboard-course"> ... </div>
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/1234/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/python.jpg" width={200} />
+            <div>
+              <h5> CS1236 Python </h5>
+              <p className="wd-dashboard-course-title">
+                Introduction to CS with Python
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/1234/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/cybersecurity.jpg" width={200} />
+            <div>
+              <h5> CS1237 Cybersecurity </h5>
+              <p className="wd-dashboard-course-title">
+                Introduction to Cybersecurity
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/1234/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/networking.jpeg" width={200} />
+            <div>
+              <h5> CS1238 Computer Networking </h5>
+              <p className="wd-dashboard-course-title">
+                Networking Fundamentals
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          {" "}
+          <Link
+            to="/Kambaz/Courses/1234/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/javascript.jpg" width={200} />
+            <div>
+              <h5> CS1239 JavaScript </h5>
+              <p className="wd-dashboard-course-title">Learning JavaScript</p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          {" "}
+          <Link
+            to="/Kambaz/Courses/1234/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/databases.jpg" width={200} />
+            <div>
+              <h5> CS1240 Relational Databases </h5>
+              <p className="wd-dashboard-course-title">
+                Database Engineering and Analytics
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
