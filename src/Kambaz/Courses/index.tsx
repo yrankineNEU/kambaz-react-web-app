509e8284ed42c101c910208dd0648d0340841c3f
yrankineNEU <rankine.y@northeastern.edu>
2025-05-13 20:11:55 -0400

    upload home elements

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -1,4 +1,7 @@
 import CourseNavigation from "./Navigation";
+import Modules from "./Modules";
+import Home from "./Home";
+
 import { Navigate, Route, Routes } from "react-router";
 
 export default function Courses() {
@@ -14,8 +17,8 @@ export default function Courses() {
           <td valign="top">
             <Routes>
               <Route path="/" element={<Navigate to="Home" />} />
-              <Route path="Home" element={<h2>Home</h2>} />
-              <Route path="Modules" element={<h2>Modules</h2>} />
+              <Route path="Home" element={<Home />} />
+              <Route path="Modules" element={<Modules />} />
               <Route path="Assignments" element={<h2>Assignments</h2>} />
               <Route
                 path="Assignments/:aid"
