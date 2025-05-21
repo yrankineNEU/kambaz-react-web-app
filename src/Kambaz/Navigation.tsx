6ba2c970487335a1c96237432c267dfb461359e9
yrankineNEU <rankine.y@northeastern.edu>
2025-05-20 23:51:02 -0400

    removed unsed imports

--- src/Kambaz/Navigation.tsx
+++ src/Kambaz/Navigation.tsx
@@ -1,7 +1,7 @@
 import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
-import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
-import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
+import { LiaBookSolid } from "react-icons/lia";
+import { FaRegCircleUser } from "react-icons/fa6";
 import { ListGroup } from "react-bootstrap";
 import { Link } from "react-router";
 import { ImLab } from "react-icons/im";
