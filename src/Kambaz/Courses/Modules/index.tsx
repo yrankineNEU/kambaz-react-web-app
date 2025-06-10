c040076f12204e48d0080c630a7c5e54fcf2e481
yrankineNEU <rankine.y@northeastern.edu>
2025-06-10 18:41:42 -0400

    update

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -33,7 +33,10 @@ export default function Modules() {
         {modules
           .filter((module: any) => module.course === cid)
           .map((module: any) => (
-            <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
+            <ListGroup.Item
+              key={module._id}
+              className="wd-module p-0 mb-5 fs-5 border-gray"
+            >
               <div className="wd-title p-3 ps-2 bg-secondary">
                 <BsGripVertical className="me-2 fs-3" />
                 {!module.editing && module.name}
@@ -63,8 +66,11 @@ export default function Modules() {
               </div>
               {module.lessons && (
                 <ListGroup className="wd-lessons rounded-0">
-                  {module.lessons.map((lesson: any) => (
-                    <ListGroup.Item className="wd-lesson p-3 ps-1">
+                  {module.lessons.map((lesson: any, index: number) => (
+                    <ListGroup.Item
+                      key={lesson._id || `${module._id}-lesson-${index}`}
+                      className="wd-lesson p-3 ps-1"
+                    >
                       <BsGripVertical className="me-2 fs-3" /> {lesson.name}
                       <LessonControlButtons />
                     </ListGroup.Item>
--- src/Labs/Lab3/DestructingImports.tsx
+++ src/Labs/Lab3/DestructingImports.tsx
@@ -6,31 +6,33 @@ export default function DestructingImports() {
       <h2>Destructing Imports</h2>
       <table className="table table-sm">
         <thead>
-          <tr>
-            <th>Math</th>
-            <th>Matematica</th>
-            <th>Functions</th>
-          </tr>
-          <tr>
-            <td>Math.add(2, 3) = {Math.add(2, 3)}</td>
-            <td>Matematica.add(2, 3) ={Matematica.add(2, 3)}</td>
-            <td>add(2, 3) = {add(2, 3)}</td>
-          </tr>
-          <tr>
-            <td>Math.subtract(5, 1) = {Math.subtract(5, 1)}</td>
-            <td>Matematica.subtract(5, 1) ={Matematica.subtract(5, 1)}</td>
-            <td>subtract(5, 1) = {subtract(5, 1)}</td>
-          </tr>
-          <tr>
-            <td>Math.multiply(3,4) = {Math.multiply(3, 4)}</td>
-            <td>Matematica.multiply(3,4) ={Matematica.multiply(3, 4)}</td>
-            <td>multiply(3,4) = {multiply(3, 4)}</td>
-          </tr>
-          <tr>
-            <td>Math.divide(8,2) = {Math.divide(8, 2)}</td>
-            <td>Matematica.divide(8,2) ={Matematica.divide(8, 2)}</td>
-            <td>divide(8,2) = {divide(8, 2)}</td>
-          </tr>
+          <tbody>
+            <tr>
+              <th>Math</th>
+              <th>Matematica</th>
+              <th>Functions</th>
+            </tr>
+            <tr>
+              <td>Math.add(2, 3) = {Math.add(2, 3)}</td>
+              <td>Matematica.add(2, 3) ={Matematica.add(2, 3)}</td>
+              <td>add(2, 3) = {add(2, 3)}</td>
+            </tr>
+            <tr>
+              <td>Math.subtract(5, 1) = {Math.subtract(5, 1)}</td>
+              <td>Matematica.subtract(5, 1) ={Matematica.subtract(5, 1)}</td>
+              <td>subtract(5, 1) = {subtract(5, 1)}</td>
+            </tr>
+            <tr>
+              <td>Math.multiply(3,4) = {Math.multiply(3, 4)}</td>
+              <td>Matematica.multiply(3,4) ={Matematica.multiply(3, 4)}</td>
+              <td>multiply(3,4) = {multiply(3, 4)}</td>
+            </tr>
+            <tr>
+              <td>Math.divide(8,2) = {Math.divide(8, 2)}</td>
+              <td>Matematica.divide(8,2) ={Matematica.divide(8, 2)}</td>
+              <td>divide(8,2) = {divide(8, 2)}</td>
+            </tr>
+          </tbody>
         </thead>
       </table>
       <hr />
