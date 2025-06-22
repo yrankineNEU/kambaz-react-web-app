6dd28f15b0b698ffe46ca6226485797a42386321
yrankineNEU <rankine.y@northeastern.edu>
2025-06-22 17:57:52 -0400

    fixing deashboard

--- src/Kambaz/store.tsx
+++ src/Kambaz/store.tsx
@@ -2,13 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./Account/reducer";
 import assignmentsReducer from "./Courses/Assignments/reducer";
 import modulesReducer from "./Courses/Modules/reducer";
+import quizzesReducer from "./Courses/Quizzes/reducer";
 
 const store = configureStore({
   reducer: {
     modulesReducer,
     accountReducer,
     assignmentsReducer,
-    // quizzesReducer,
+    quizzesReducer,
   },
 });
 export default store;
