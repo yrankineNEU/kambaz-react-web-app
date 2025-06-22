e96939b66fa9846adb3941768df944b6965b5832
yrankineNEU <rankine.y@northeastern.edu>
2025-06-21 21:41:03 -0400

    add axiosWithCredentials to quiz client

--- src/Kambaz/Courses/Quizzes/client.tsx
+++ src/Kambaz/Courses/Quizzes/client.tsx
@@ -1,30 +1,41 @@
 import axios from "axios";
+const axiosWithCredentials = axios.create({ withCredentials: true });
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 const QUIZZES_API = `${REMOTE_SERVER}/api/quizzes`;
 
 export const findQuizzesForCourse = async (courseId: string) => {
-  const response = await axios.get(`${COURSES_API}/${courseId}/quizzes`);
+  const response = await axiosWithCredentials.get(
+    `${COURSES_API}/${courseId}/quizzes`
+  );
   return response.data;
 };
 
 export const createQuizForCourse = async (courseId: string, quiz: any) => {
-  const response = await axios.post(`${COURSES_API}/${courseId}/quizzes`, quiz);
+  const response = await axiosWithCredentials.post(
+    `${COURSES_API}/${courseId}/quizzes`,
+    quiz
+  );
   return response.data;
 };
 
 export const updateQuiz = async (quiz: any) => {
-  const response = await axios.put(`${QUIZZES_API}/${quiz._id}`, quiz);
+  const response = await axiosWithCredentials.put(
+    `${QUIZZES_API}/${quiz._id}`,
+    quiz
+  );
   return response.data;
 };
 
 export const deleteQuiz = async (quizId: string) => {
-  const response = await axios.delete(`${QUIZZES_API}/${quizId}`);
+  const response = await axiosWithCredentials.delete(
+    `${QUIZZES_API}/${quizId}`
+  );
   return response.data;
 };
 
 export const findQuizById = async (quizId: string) => {
-  const response = await axios.get(`${QUIZZES_API}/${quizId}`);
+  const response = await axiosWithCredentials.get(`${QUIZZES_API}/${quizId}`);
   return response.data;
 };
 
@@ -32,8 +43,11 @@ export const toggleQuizPublished = async (
   quizId: string,
   published: boolean
 ) => {
-  const response = await axios.put(`${QUIZZES_API}/${quizId}/publish`, {
-    published,
-  });
+  const response = await axiosWithCredentials.put(
+    `${QUIZZES_API}/${quizId}/publish`,
+    {
+      published,
+    }
+  );
   return response.data;
 };
