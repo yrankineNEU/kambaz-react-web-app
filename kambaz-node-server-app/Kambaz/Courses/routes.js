a645d9c502fa26fbae2ae83ddbc23663ac6758ff
yrankineNEU <rankine.y@northeastern.edu>
2025-06-22 19:06:20 -0400

    fixing routes for course

--- kambaz-node-server-app/Kambaz/Courses/routes.js
+++ kambaz-node-server-app/Kambaz/Courses/routes.js
@@ -5,7 +5,7 @@ import * as dao from "./dao.js";
 export default function CourseRoutes(app) {
   app.get("/api/courses", async (req, res) => {
     const courses = await dao.findAllCourses();
-    res.json(courses);
+    res.json(courses.filter((course) => course !== null));
   });
 
   const findUsersForCourse = async (req, res) => {
